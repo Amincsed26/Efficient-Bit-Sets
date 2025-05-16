5a61a7648f492635e19b60d5137795d3c13de187
Amin Mohamed <145727573+Amincsed26@users.noreply.github.com>
2025-05-16 12:22:19 +0300

    Delete SetDS directory

--- SetDS/IBitOperations.java
+++
@@ -1,6 +0,0 @@
-public interface IBitOperations {
-    public int getBit(int number, int position);
-    public int setBit(int number, int position);
-    public int clearBit(int number, int position);
-    public int updateBit(int number, int position, boolean value) ;
-}
--- SetDS/ISet.java
+++
@@ -1,9 +0,0 @@
-public interface ISet {
-    public int union(Set other);
-    public int intersection(Set other);
-    public int complement();
-    public int difference(Set other);
-    public int cardinality();
-    public void print();
-
-}
